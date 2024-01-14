e971c39ab997a23170d814968d3a7c1f94b09be6
undisclosed64 <undisclosed64@gmail.com>
2024-01-13 23:30:13 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Movies } from "./components/Movies";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Movies></Movies>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    colors: {
+      darkblue: "#111827",
+      black: "#030712",
+      yellow: "#ffd62c",
+      darkgrey: "#748394",
+      darkblue2: "#323945",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
