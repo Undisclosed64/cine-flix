3e0703b7789ae0cf6690d477c0b10c68be836856
undisclosed64 <undisclosed64@gmail.com>
2024-01-16 01:59:30 +0600

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import { RiMovie2Line } from "react-icons/ri";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { PiShoppingCartLight } from "react-icons/pi";
-import { FaRegCircleUser } from "react-icons/fa6";
 import { PiUserCircleLight } from "react-icons/pi";
+import { useState } from "react";
+import Cart from "./Cart";
 
 export const Navbar = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const openCart = () => {
+    setShowCart(true);
+  };
+  const closeCart = () => {
+    setShowCart(false);
+  };
   return (
     <div className="header bg-darkblue py-2 px-10 flex justify-between">
       <div className="text-2xl text-white flex items-center gap-2">
@@ -13,9 +22,10 @@ export const Navbar = () => {
       </div>
       <div className="icons text-darkgrey flex items-center gap-4 text-2xl">
         <IoIosNotificationsOutline className="" />
-        <PiShoppingCartLight />
+        <PiShoppingCartLight onClick={openCart} />
         <PiUserCircleLight />
       </div>
+      {showCart ? <Cart closeCart={closeCart} /> : ""}
     </div>
   );
 };
