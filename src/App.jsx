13d7a685dc7b5dff2da47a41973fdd8d9f3dac56
undisclosed64 <undisclosed64@gmail.com>
2024-01-16 02:01:44 +0600

    use cart provided

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Movies } from "./components/Movies";
+import { CartProvider } from "./CartContext";
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Navbar></Navbar>
       <Movies></Movies>
-    </>
+    </CartProvider>
   );
 }
 
