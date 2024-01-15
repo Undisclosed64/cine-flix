23472a81f97fa4c59faef78ec8e9d6a9d34dd841
undisclosed64 <undisclosed64@gmail.com>
2024-01-16 01:59:45 +0600

    add movies

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { PiShoppingCartBold } from "react-icons/pi";
 import { IoStar } from "react-icons/io5";
+import { MovieModal } from "./MovieModal";
+import { useCartContext } from "../CartContext";
 
 export const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [genre, setGenres] = useState([]);
+  const [openModals, setOpenModals] = useState({});
+  const { addMovie } = useCartContext();
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -24,6 +28,20 @@ export const Movies = () => {
     fetchMovies();
   }, []);
 
+  const openModal = (movieId) => {
+    console.log("clicked");
+    setOpenModals((prevModals) => ({
+      ...prevModals,
+      [movieId]: true,
+    }));
+  };
+
+  const closeModal = (movieId) => {
+    setOpenModals((prevModals) => ({
+      ...prevModals,
+      [movieId]: false,
+    }));
+  };
   useEffect(() => {
     const fetchGenres = async () => {
       const response = await axios.get(
@@ -39,7 +57,13 @@ export const Movies = () => {
     };
     fetchGenres();
   }, []);
-  console.log(genre);
+
+  const handleAddToCart = (e, movie) => {
+    e.stopPropagation();
+    movie.price = 16.99;
+    addMovie(movie);
+  };
+
   return (
     <div className="movies-wrapper grid grid-cols-4 gap-4 p-10 bg-black">
       {movies.map((movie) => {
@@ -51,6 +75,7 @@ export const Movies = () => {
           <div
             key={movie.id}
             className=" bg-darkblue3 rounded border border-darkblue"
+            onClick={() => openModal(movie.id)}
           >
             <img
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
@@ -58,7 +83,7 @@ export const Movies = () => {
               className="rounded-t"
             />
 
-            <div className="content p-3 flex flex-col">
+            <div className="content p-3 flex flex-col justify-between">
               <h2 className="text-[#e6e8e9] mb-2">{movie.original_title}</h2>
               <div className="sub-group text-darkgrey flex items-center text-sm gap-2 mb-4">
                 <div className="genre">
@@ -74,28 +99,33 @@ export const Movies = () => {
                 <span className="bg-darkblue2 w-1 h-1 rounded-full"></span>
                 <div className="price">$16.99</div>
               </div>
-              <div className="buttons text-darkgrey flex gap-2 mt-auto">
-                <button className=" border bg-yellow text-black p-2 rounded font-semibold flex items-center justify-center gap-1 w-1/2">
+              <div className="buttons text-darkgrey flex gap-2 items-end">
+                <button
+                  className=" border bg-yellow text-black p-2 rounded font-semibold flex items-center justify-center gap-1 w-1/2"
+                  onClick={(e) => handleAddToCart(e, movie)}
+                >
                   <PiShoppingCartBold className="text-xl" />
                   Add
                 </button>
-                <button className="w-1/2 border  border-darkblue2 p-2 rounded text-[#e6e8e9]">
+                <button
+                  className="w-1/2 border  border-darkblue2 p-2 rounded text-[#e6e8e9]"
+                  onClick={() => openModal(movie.id)}
+                >
                   View details
                 </button>
               </div>
             </div>
+            {openModals[movie.id] ? (
+              <MovieModal
+                id={movie.id}
+                closeModal={() => closeModal(movie.id)}
+              />
+            ) : (
+              ""
+            )}
           </div>
         );
       })}
     </div>
   );
 };
-// {genre.genres
-//     .filter((item) => movie.genre_ids.includes(item.id))
-//     .map((matchedGenre, index, array) => (
-//       <span key={matchedGenre.id}>
-//         {matchedGenre.name}
-//         {index < array.length - 1 && ", "}
-//       </span>
-//     ))}
-//bg-[#69707c] dot color
